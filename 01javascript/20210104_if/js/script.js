cdf53bcc7ca3f221c793c891948efa2479612de0
kmd2410 <75105354+kmd2410@users.noreply.github.com>
2021-01-13 13:44:20 +0900

    up

---
+++ 01javascript/20210104_if/js/script.js
@@ -0,0 +1,7 @@
+let id,password;
+id = prompt();
+
+if (id=="admin"){//document.write("관리자 입니다.");
+	location.href="login.html"}
+	else{//document.write("관리자가 아닙니다. 나가세요");
+	location.href="error.html"}
